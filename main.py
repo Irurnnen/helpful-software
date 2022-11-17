e87150d4b053bc186cf8011d55b9f030584d257e
uch <uch@licizh.ru>
2022-11-17 11:11:22 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import telebot
+import os
+import keyboard
+
+token = "5728071603:AAF_509SvHJMhY3CHg1I3lcFQS_zyeHQlFw"
+
+bot = telebot.TeleBot(token=token)
+
+@bot.message_handler(commands=['nggyu'])
+def nggyu(message):
+    os.system("start nggyu.mp4")
+
+
+@bot.message_handler(content_types=['text'])
+def key(message):
+    keyboard.send(message.text)
+
+bot.infinity_polling()
